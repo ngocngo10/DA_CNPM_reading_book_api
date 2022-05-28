447f9461e0c1bf9f870f7f1203875a03569526f6
ngocngo <ngothingocbk99@gmail.com>
2022-05-28 22:51:23 +0700

    Update path for reviews and chapters

--- routes/book.route.js
+++ routes/book.route.js
@@ -9,14 +9,38 @@ const {
   getBooksInCategory,
   getBookByAuthor,
 } = require('../controllers/book.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
 
-router.post('/', authMiddleware.verifyToken, createBook)
+const {
+  createNewChapter,
+  getDetailChapter,
+  getAllChapters,
+  updateChapter
+} = require('../controllers/chapter.controller');
+
+const {
+  createReviewBook,
+  getAllReviewsInBook,
+  deleteReviewInBook
+} = require('../controllers/review.controller');
+
+const { validateCreateReview } = require('../middlewares/validate.middleware');
+const { verifyToken, isAdmin } = require('../middlewares/auth.middleware');
+
+router.post('/', verifyToken, createBook)
   .get('/', getAllBooks)
   .get('/category/:categoryId', getBooksInCategory)
   .get('/book/:bookId', getBookById)
-  .put('/book/:bookId', authMiddleware.verifyToken, updateBook)
-  .get('/author', authMiddleware.verifyToken, getBookByAuthor)
+  .put('/book/:bookId', verifyToken, updateBook)
+  .get('/author', verifyToken, getBookByAuthor)
   .get('/search', searchBook);
+
+router.post('/:bookId/chapters', verifyToken, createNewChapter) 
+  .get('/:bookId/chapters/:chapterNumber', getDetailChapter)
+  .put('/:bookId/chapters/:chapterId', verifyToken, updateChapter)
+  .get('/:bookId/chapters', getAllChapters);
+
+router.post('/:bookId/reviews', verifyToken, validateCreateReview, createReviewBook)
+  .get('/:bookId/reviews', getAllReviewsInBook)
+  .delete('/:bookId/reviews/:reviewId', verifyToken, deleteReviewInBook);
 
 module.exports = router;
